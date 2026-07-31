7927599f955dc7ac231750cbbd5ce3c90d2302ef
excellentincompetent <69798516+excellentincompetent@users.noreply.github.com>
2026-07-31 23:00:04 +0300

    firstCommit basics
    + grid map
    + basic move
    + jump
    + sprint
    + camera

---
+++ Scripts/character_player_movement.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 4.5
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Sprint toggle
+	var current_speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else SPEED
+
+	# Movement input
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
